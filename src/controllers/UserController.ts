8f2b55f031becc1377a8662c1c377f781979496b
Alex Silva <9apsilva@gmail.com>
2022-05-06 14:56:23 -0300

    feat(userController): add method create user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,41 @@
+import { Request, Response } from 'express'
 import { prisma } from '../Db/PrismaClient'
 
 
 export class UserController {
 
-    async get() {
+    async get(res: Response) {
         const users = await prisma.user.findMany()
-        return users
+
+        if (users[0] == null) return res.status(200).json({ message: 'users not found' })
+        return res.status(200).send(users)
+    }
+
+    async create(req: Request, res: Response) {
+        const { name, email } = req.body
+        
+        if(!name || !email) return res.status(400).json({error: 'name and email is required'})
+
+        try {
+            const userAlreadyExists = await prisma.user.findFirst({
+                where: {
+                    email: email
+                }
+            })
+
+            if(userAlreadyExists) return res.status(400).json({error: 'this email already exists'})
+            
+            await prisma.user.create({
+                data: {
+                    name: name,
+                    email: email
+                }
+            })
+
+            return res.status(201).json({ message: 'user successfully created' })
+        }
+        catch(err){
+            return res.status(400).json({error: `failed to create user: ${err}`})
+        }
     }
 }
--- src/router/users.ts
+++ src/router/users.ts
@@ -4,8 +4,13 @@ import { UserController } from '../controllers/UserController'
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    const users = await new UserController().get()
-    res.send(users);
+    await new UserController().get(res)
+    return res.end()
+})
+
+router.post('/create', async (req, res) => {
+    await new UserController().create(req, res)
+    return res.end()
 })
 
 export { router }
