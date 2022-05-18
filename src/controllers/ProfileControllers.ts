5dc9efc9f00a19da33413aa3a247935a40437b22
Alex Silva <9apsilva@gmail.com>
2022-05-18 16:49:57 -0300

    feat(Profiles): add resources and routes

--- src/controllers/ProfileControllers.ts
+++ src/controllers/ProfileControllers.ts
@@ -4,16 +4,16 @@ import { regex } from '../auxiliaryFunction/Regex'
 
 type profile = {
     name: string,
-    bio: string
+    bio?: string
 }
 
 export class Profile {
 
-    async create(req: Request, res: Response) {
+    async createProfile(req: Request, res: Response) {
         const _userId = req.params.id
         const { name, bio }: profile = req.body
-        
-        if(!regex.name(name)) return res.status(400).json({error: 'name contains forbidden characters'})
+
+        if (!regex.name(name)) return res.status(400).json({ error: 'name contains forbidden characters' })
 
         try {
             await prisma.user.update({
@@ -32,8 +32,56 @@ export class Profile {
             return res.status(201).json({ message: 'profile successfully created' })
         }
         catch (err) {
-            if(!_userId) return res.status(401).json({error: 'token invalid'})
-            return res.status(405).json({error: 'method not allowed'})
+            if (!_userId) return res.status(401).json({ error: 'token invalid' })
+            return res.status(405).json({ error: 'already exists a profile for this user' })
+        }
+    }
+
+    async getAllProfiles(req: Request, res: Response) {
+        const profiles = await prisma.profile.findMany()
+        if (!profiles) return res.json({ message: 'no registered profile' })
+        return res.send(profiles)
+    }
+
+    async getOneProfile(req: Request, res: Response) {
+        const id = req.params.id
+
+        try {
+            const profile = await prisma.profile.findFirst({
+                where: {
+                    userId: Number(id)
+                }
+            })
+            if (!profile) return res.json({ message: 'this user yet has no profile' })
+            return res.send(profile)
+        }
+        catch (err) {
+            return res.sendStatus(500)
+        }
+    }
+
+    async updateProfile(req: Request, res: Response) {
+        const id = req.params.id
+        const { name, bio }: profile = req.body
+
+        if (!bio || !name) return res.status(400).json({ error: 'there is empty field' })
+        if (!regex.name(name)) return res.status(400).json({ error: 'name contains forbidden characters' })
+
+        try {
+            const profile = await prisma.profile.update({
+                where: {
+                    userId: Number(id)
+                },
+                data: {
+                    name,
+                    bio
+                }
+            })
+
+            return res.send(profile)
+        }
+        catch (err) {
+            return res.status(304).json({ error: 'update failed' })
         }
     }
 
--- src/router/profiles.ts
+++ src/router/profiles.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { Authentication } from '../controllers/AuthMiddleware'
 import { Profile } from '../controllers/ProfileControllers'
 
-const router = Router()
 
-router.post('/', Authentication, async (req, res) => {
-    await new Profile().create(req, res)
-    res.end()
-})
+const router = Router()
+const profile = new Profile()
+
+router.post('/', Authentication, profile.createProfile)
+router.get('/', Authentication, profile.getOneProfile)
+router.get('/all', profile.getAllProfiles)
+router.put('/', Authentication, profile.updateProfile)
 
 export { router as profiles }
